b24222f7bce63d51283b6c972a5bbf93ab9954b5
Joe Jackson <joe.jackson@scimendsolutions.com>
2013-10-05 11:16:36 -0400

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'nanoc'
+gem 'adsf'
+gem 'kramdown'
+gem 'susy'
+gem "guard-nanoc"
---
+++ compass/config.rb
@@ -0,0 +1,12 @@
+require 'susy'
+
+http_path    = "/" 
+project_path = "." 
+css_dir      = "output/stylesheets"
+sass_dir     = "content/assets/stylesheets"
+images_dir   = "output/images"
+
+# when using SCSS:
+sass_options = {
+  syntax: :scss
+}
